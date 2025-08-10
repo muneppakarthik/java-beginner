99eb8eeab505f572d197afe220dfce611ef8f5e0
M Karthik <139629620+muneppakarthik@users.noreply.github.com>
2025-08-10 19:36:41 +0530

    java relational-operators

---
+++ java-telusko/4.java-relational-operators/index.java
@@ -0,0 +1,15 @@
+class RelationalOperator{
+    public static void main(String args[]){
+        int x = 6;
+        int y = 5;
+
+        // boolean result = x<y; // false
+        // boolean result = x>y; // true
+        // boolean result = x>=y; // false
+        // boolean result = x<=y; // false
+        // boolean result = x!=y; // true
+        boolean result = x==y; // false
+
+        System.out.println(result);
+    }
+}
