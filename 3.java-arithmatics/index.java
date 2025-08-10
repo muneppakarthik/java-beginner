9451ac04055e5a4557b184b01c75d17286163db4
M Karthik <139629620+muneppakarthik@users.noreply.github.com>
2025-08-10 19:18:53 +0530

    java arithmatcs

---
+++ 3.java-arithmatics/index.java
@@ -0,0 +1,18 @@
+class Arithmatic{
+    public static void main(String args[]){
+        int num1 = 7;
+        int num2 = 5;
+        // int result = num1 + num2; // add
+        // int result = num1 - num2; // sub
+        // int result = num1 * num2; // multi
+        // int result = num1 / num2; // division
+        // int result = num1 % num2; // reminder or modulus
+        // num1 += 3; // shortcut
+        // num1++; // shortcut
+
+        // int result = num1++; //post increment
+        int result = ++num1; //before increment
+
+        System.out.println(result);
+    }
+}
