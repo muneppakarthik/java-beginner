c415fa92dfb3e693fe4924f4383ab9668e69b19d
M Karthik <139629620+muneppakarthik@users.noreply.github.com>
2025-08-10 18:14:07 +0530

    java datatype

---
+++ Hello.java
@@ -0,0 +1,13 @@
+class Hello{
+    public static void main(String args[]){
+        int num = 3;
+        byte by = 127;
+        String name = "karthik";
+        float f = 1223.4f;
+        char a = 'a';
+        a++;
+        System.out.println(num);
+        System.out.println(10+90);
+        System.out.println(a);
+    };
+};
