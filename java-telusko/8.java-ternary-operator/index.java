80c98c6f9679e239b3f915ade0c8ce61a8db7426
M Karthik <139629620+muneppakarthik@users.noreply.github.com>
2025-08-11 12:04:25 +0530

    java ternary-operator

---
+++ java-telusko/8.java-ternary-operator/index.java
@@ -0,0 +1,17 @@
+class TernaryOperator{
+    public static void main(String args[]){
+        int n = 6;
+        int result = 0;
+
+        // if(n%2 == 0)
+        //     result = 10;
+        // else
+        //     result = 20;
+
+        // ?: ternary opertor
+
+        result = n%2==0 ? 10 : 20;
+        
+        System.out.println(result);
+    }
+}
