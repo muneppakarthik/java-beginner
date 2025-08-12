c7a04a1e422bc1d8bc02986413d9f128e5b5b8de
M Karthik <139629620+muneppakarthik@users.noreply.github.com>
2025-08-12 16:20:05 +0530

    java string-buffer

---
+++ java-telusko/20.java-string-buffer/StringBuffers.java
@@ -0,0 +1,8 @@
+class StringBuffers{
+    public static void main(String args[]){
+        StringBuffer sb = new StringBuffer("Karthik");
+        // StringBuffer are mutable data - Which can be change
+        sb.append(" Muneppa");
+        System.out.println(sb);
+    };
+};
