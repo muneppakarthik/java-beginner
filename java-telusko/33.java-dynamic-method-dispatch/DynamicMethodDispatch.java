30df535c8e73b47a0120f364f68f80b94a6aabbd
M Karthik <139629620+muneppakarthik@users.noreply.github.com>
2025-08-14 15:11:48 +0530

    java dynamic-dispatch

---
+++ java-telusko/33.java-dynamic-method-dispatch/DynamicMethodDispatch.java
@@ -0,0 +1,30 @@
+class DynamicMethodDispatch{
+    public static void main(String args[]){
+        A obj = new A();
+        obj.show();
+
+        obj = new B();
+        obj.show();
+
+        obj = new C();
+        obj.show();
+    };
+};
+
+class A{
+    public void show(){
+        System.out.println("in a");
+    }
+}
+
+class B extends A{
+    public void show(){
+        System.out.println("in b");
+    }
+}
+
+class C extends A{
+    public void show(){
+        System.out.println("in c");
+    }
+}
